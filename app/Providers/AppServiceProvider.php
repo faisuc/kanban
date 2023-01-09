659d7cafad88d5b2d03013b4c075c4486fabce2c
Neil Carlo Sucuangco <succute@yahoo.com>
2023-01-10 04:26:28 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+        
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
